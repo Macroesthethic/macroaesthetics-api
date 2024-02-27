83db67b3b1189dcd9cadc2a9cc5e4e25dd71ba77
FractionOfCode <javierholguin596.com>
2024-02-27 11:53:34 -0500

    ADD: Finishing configuring communication with the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,35 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), 
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+
+      ssl: process.env.SSL_MODE === 'true',
+      extra: {
+        ssl:
+          process.env.SSL_MODE === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
+    
+    }),
+    
+    AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
