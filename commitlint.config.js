7d0d542255ff4e6093fb356f0d7f250f2555eb1b
Ubuntu <ubuntu@ip-172-31-24-104.us-east-2.compute.internal>
2024-05-02 22:47:27 +0000

    feat : lintstaged and commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-	extends : ['@commitlint/config-conventional']
-}
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+      ],
+    ],
+  },
+};
