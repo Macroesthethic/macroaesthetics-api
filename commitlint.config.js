d9bcd1bcbd9b0eaf476a3a055a69885c034324f3
Ubuntu <ubuntu@ip-172-31-24-104.us-east-2.compute.internal>
2024-05-02 22:39:44 +0000

    FIX : husky config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,3 @@
 module.exports = {
 	extends : ['@commitlint/config-conventional']
-	rules : {
-		'import/no-unused-modules' : 'error',
-		'no-var' : 'error',
-		'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
-		'scope-case': [2, 'always', 'lower-case'],
-		'subject-case': [2, 'always', 'sentence-case']
-	}
 }
